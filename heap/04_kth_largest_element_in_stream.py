5648e32a40f45b39718e8336b456a4ba4157e44a
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2026-08-25 05:53:04 +0700

    kth largest element in stream

---
+++ heap/04_kth_largest_element_in_stream.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/kth-largest-element-in-a-stream/description
+
+import heapq
+
+
+class KthLargest:
+    # Time complexity: nlog(k)
+    # space complexity: O(k)
+
+    def __init__(self, k: int, nums: List[int]):
+        self.minHeap = []
+        self.lenHeap = k
+        for num in nums:
+            heapq.heappush(self.minHeap, num)
+            if len(self.minHeap) > k:
+                heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.lenHeap:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
